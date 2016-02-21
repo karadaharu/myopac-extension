fc6fa79f7b2f4a6e04ca543dfa476e4cf39f5456
Kazumasa <catter121@gmail.com>
2016-02-22 01:19:47 +0900

    パス修正

--- check.php
+++ check.php
@@ -5,7 +5,7 @@ require_once APP_ROOT . '/vendor/autoload.php';
 require_once APP_ROOT . '/lib/MyOpac.php';
 
 $myopac = new MyOpac();
-$json = file_get_contents( APP_ROOT . '/users.json');
+$json = file_get_contents( APP_ROOT . '/data/users.json');
 $json = mb_convert_encoding($json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
 $users = json_decode($json, true)['users'];
 foreach ($users as $user) {
